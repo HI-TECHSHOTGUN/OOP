bb868413975fd20acdaaefd57cefb431024c49a1
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-02-14 19:50:49 +0300

    tests maybe done

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+
+from main import Product, Category
+
+
+"""Фикстуры для тестирования"""
+@pytest.fixture
+def sample_product():
+    return Product("Test Product", "Test Description", 20, 10)
+
+@pytest.fixture
+def sample_product2():
+    return Product("Another Product", "Another Description", 30, 5)
+
+@pytest.fixture
+def sample_category(sample_product):
+    return Category("Test Category", "Test Category Description", [sample_product])
+
