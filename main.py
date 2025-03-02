e15ab0a6db4c86d953e102a54b246c5f4d2e483f
HI-TECHSHOTGUN <maksimov_maksime@mail.ru>
2025-03-02 21:37:49 +0300

    HW_done

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f'{self.name}, {self.price} руб. Остаток: {self.quantity} шт.'
+
+    def __add__(self, other):
+        return self.quantity * self.price + other.quantity * other.price
+
     @classmethod
     def new_product(cls, _dict):
         try:
@@ -47,6 +53,10 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(self.__products) if products else 0
 
+    def __str__(self):
+        total_quantity = sum(i.quantity for i in self.__products)
+        return f'{self.name}, количество продуктов: {total_quantity} шт.'
+
     def add_product(self, product):
         if isinstance(product, Product):
             self.__products.append(product)
@@ -62,10 +72,14 @@ class Category:
         return result
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
 
     category1 = Category(
         "Смартфоны",
@@ -73,24 +87,10 @@ if __name__ == "__main__":
         [product1, product2, product3]
     )
 
+    print(str(category1))
+
     print(category1.products)
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
 
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5})
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product.price = 800
-    print(new_product.price)
-
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
